3a36fe3a23a9859b84bc06c8fda4baa02b4388d0
nrod42 <nigelrod94@gmail.com>
2022-11-04 16:39:52 -0400

    added script links to bootstrap to get hamburger button working

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// const body = document.querySelector('body');
+// const toggler = document.querySelector('.navbar-toggler');
+// const nav = document.getElementById('navbarNav')
+
+// body.addEventListener('click', () => {
+//     toggler.className="navbar-toggler collapsed";
+//     toggler.setAttribute("aria-expanded", false);
+//     nav.className = 'navbar-collapse collapse';
+// })
